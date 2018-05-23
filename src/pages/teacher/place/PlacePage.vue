4731cd5a561430be1cd983e0549b2e80c92b18a7
fzygun <fzygun@qq.com>
2018-05-23 22:01:20 +0800

    课室页面

---
+++ src/pages/teacher/place/PlacePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="index-place">
+    <div class="index-place--title">
+      <p>南京市仙林校区</p>
+      <div class="index-place--title-icon"></div>
+    </div>
+    <div class="index-place--title-desc">
+      <span>人气10288</span><span>累计开课278</span><span>距离你3.5km</span>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+  @include b(index-place) {
+    @include m(title) {
+      display: flex;
+      padding-top: 0.38rem;
+      font-size: $--common-headline-font-size;
+      align-items: center;
+      >p{
+        flex-grow: 1;
+      }
+    }
+    @include m(title-icon) {
+      background: $--place-headline-icon-background;
+      background-size: 100% 100%;
+      width: 0.88rem;
+      height: 0.88rem;
+    }
+    @include m(title-desc) {
+      font-size: $--common-illustration-text-font-size;
+    }
+  }
+</style>
