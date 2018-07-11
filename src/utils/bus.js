36fa2de5b72adae689d5c2e4bc5c38ac03760d5c
zhangyiyong <287069934@qq.com>
2018-07-11 14:15:44 +0800

    bugfix: bus公共组件修复

--- src/utils/bus.js
+++ src/utils/bus.js
@@ -2,22 +2,16 @@
 import Vue from 'vue'
 const BaseBus = new Vue()
 export default {
-	$on(type, callback, vm) {
-		BaseBus.$on(type, callback)
+	$on(eventName, callback, vm) {
+		BaseBus.$on(eventName, callback)
 		// vm 若为 vue实例，则注册如果组件销毁时，自动注销的组件，避免组件每次懂需手动添加组件注销时，注销注册的事件
 		if (vm && vm instanceof Vue) {
 			Vue.config.optionMergeStrategies.destroyed(vm.$options.destroyed, function() {
-				BaseBus.$off(type, callback)
+				BaseBus.$off(eventName, callback)
 			})
 		}
 	},
-	$emit(type) {
-		BaseBus.$emit(type)
-	},
-	$once(type, callback) {
-		BaseBus.$once(type, callback)
-	},
-	$off(type, callback) {
-		BaseBus.$off(type, callback)
-	}
+	$once: BaseBus.$once,
+	$off: BaseBus.$off,
+	$emit: BaseBus.$emit
 }
